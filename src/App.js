fa239c8c3386a6da659c1ed4d3774c357a188433
William Pernalete <william.pernalete@gmail.com>
2020-06-10 20:53:43 -0400

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Header titulo="Cotizador de Seguros" />
+      <ContenedorFormulario>
+        <Formulario />
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+
+const Campo = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+  display: block;
+  padding: 1rem;
+  width: 100%;
+  border: 1px solid #e1e1e1;
+  --webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
+
+const Boton = styled.button`
+  background-color: #00838f;
+  font-size: 16px;
+  width: 100%;
+  padding: 1rem;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  transition: background-color 0.3s ease;
+  margin-top: 2rem;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #26c6da;
+  }
+`;
+
+const Formulario = () => {
+  const [datos, guardarDatos] = useState({
+    marca: "",
+    year: "",
+    plan: "",
+  });
+
+  //extraer los valores del state destructuring
+  const { marca, year, plan } = datos;
+
+  //guardamos los datos en el state
+  const obtenerInformacion = (e) => {
+    guardarDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <form>
+      <Campo>
+        <Label>Marca</Label>
+        <Select name="marca" value={marca} onChange={obtenerInformacion}>
+          <option value="">-- Seleccione --</option>
+          <option value="americano">Americano</option>
+          <option value="americano">Europeo</option>
+          <option value="americano">Asiatico</option>
+        </Select>
+      </Campo>
+
+      <Campo>
+        <Label>Año</Label>
+        <Select name="year" value={year} onChange={obtenerInformacion}>
+          <option value="">-- Seleccione --</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+          <option value="2017">2017</option>
+          <option value="2016">2016</option>
+          <option value="2015">2015</option>
+          <option value="2014">2014</option>
+          <option value="2013">2013</option>
+          <option value="2012">2012</option>
+        </Select>
+      </Campo>
+
+      <Campo>
+        <Label>Plan</Label>
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="basico"
+          checked={plan === "basico"}
+          onChange={obtenerInformacion}
+        />{" "}
+        Básico
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="completo"
+          checked={plan === "completo"}
+          onChange={obtenerInformacion}
+        />{" "}
+        Completo
+      </Campo>
+
+      <Boton type="button">Cotizar</Boton>
+    </form>
+  );
+};
+
+export default Formulario;
